bcf5b1e308f741f2a2c92d14ff99057b662b34f3
uchino <44802818+kk-uchino@users.noreply.github.com>
2022-12-08 23:15:41 +0900

    プラグインの説明を追加

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $title = 'Discord通知プラグイン';
-$description = '';
-$author = '';
+$description = 'メールフォームの送信をDiscordに通知できるプラグインです。';
+$author = 'Koki Uchino';
 $url = '';
 $adminLink = ['plugin' => 'discord_notify', 'controller' => 'discord_notify', 'action' => 'config'];
